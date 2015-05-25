b9b4b64ef6b7048f8a738fb348ae6fac4c63f4cd
zhangxi <zhangxi1014@gmail.com>
2015-05-26 03:23:34 +0800

    commit

---
+++ xcode/p1000/p1000/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  p1000
+//
+//  Created by zhangxi on 5/26/15.
+//  Copyright (c) 2015 zhangxi. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    int a, b;
+    int sum;
+    cin >> a >> b;
+    sum = a + b;
+    // insert code here...
+    cout << "the sum of " << a << " and " << b << " is "<< sum << endl;
+    return 0;
+}
